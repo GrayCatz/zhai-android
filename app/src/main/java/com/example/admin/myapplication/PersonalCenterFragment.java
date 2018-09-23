bf794625a707765eabc959b7128679bef9e9a6c2
yzj <zhijian.ye@skysolutions.cn>
2018-09-24 03:07:41 +0800

    初始化项目

--- app/src/main/java/com/example/admin/myapplication/PersonalCenterFragment.java
+++ app/src/main/java/com/example/admin/myapplication/PersonalCenterFragment.java
@@ -17,6 +17,7 @@ public class PersonalCenterFragment extends Fragment {
     QMUIGroupListView mGroupListView;
     QMUITopBarLayout mTopBar;
 
+//    QMUIRadiusImageView mRadiusImageView;
 
     @Nullable
     @Override
